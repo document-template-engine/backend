eb1d1fc9953da0313070ffdfaf729b21456905da
Alexander Timchenko <astimch2022@gmail.com>
2023-10-03 22:20:31 +0300

    feature: core application added

---
+++ backend/core/template_render.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+
+def docx_render(path: str, context: Dict[str, str]):
+    """
+    Выполняет рендеринг шаблона согласно предоставленному контексту.
+
+    path - путь к шаблону
+    context - словарь подстановок
+    """
+    pass
