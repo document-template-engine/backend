822b233ba7732a904b16a6c0613506aef698256e
Nikita Romanov <RNN@pqegroup.com>
2023-10-14 14:35:25 +0300

    k

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserSerializer
 from rest_framework import serializers
+from django.db import transaction
 
 User = get_user_model()
 
@@ -8,36 +9,9 @@ from documents.models import (
     Document,
     DocumentField,
     Template,
-    TemplateField
+    TemplateField,
+    FieldToDocument
     )
-
-
-class TemplateSerializer(serializers.ModelSerializer):
-    """ Заглушка. Сериализатор шаблонов. """
-    class Meta:
-        model = Template
-        fields = '__all__'
-
-
-class TemplateFieldSerializer(serializers.ModelSerializer):
-    """ Заглушка. Сериализатор поля шаблона. """
-    class Meta:
-        model = TemplateField
-        fields = '__all__'
-
-
-class DocumentSerializer(serializers.ModelSerializer):
-    """ Заглушка. Сериализатор документов. """
-    class Meta:
-        model = Document
-        fields = '__all__'
-
-
-class DocumentFieldSerializer(serializers.ModelSerializer):
-    """ Заглушка. Сериализатор поля документов. """
-    class Meta:
-        model = DocumentField
-        fields = '__all__'
 
 
 class CustomUserSerializer(UserSerializer):
@@ -59,3 +33,86 @@ class CustomUserSerializer(UserSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+
+class TemplateSerializer(serializers.ModelSerializer):
+    """ Заглушка. Сериализатор шаблонов. """
+    class Meta:
+        model = Template
+        fields = '__all__'
+
+
+class TemplateFieldSerializer(serializers.ModelSerializer):
+    """ Заглушка. Сериализатор поля шаблона. """
+    class Meta:
+        model = TemplateField
+        fields = '__all__'
+
+
+class DocumentFieldSerializer(serializers.ModelSerializer):
+    """ Сериализатор поля документов. """
+    class Meta:
+        model = DocumentField
+        fields = '__all__'
+
+
+class DocumentReadSerializer(serializers.ModelSerializer):
+    """ Сериализатор документов. """
+    document_fields = DocumentFieldSerializer(many=True)
+    class Meta:
+        model = Document
+        fields = (
+            'id',
+            'created',
+            'completed',
+            "description",
+            'template_id',
+            'owner',
+            'document_fields'
+        )
+
+
+class DocumentWriteSerializer(serializers.ModelSerializer):
+    """ Сериализатор документов. """
+    document_fields = DocumentFieldSerializer(many=True)
+    class Meta:
+        model = Document
+        fields = (
+            'id',
+            'created',
+            'completed',
+            "description",
+            'template_id',
+            'document_fields'
+        )
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        """ Пока говно код. Создание документа и полей документа """
+        document_fields = validated_data.pop("document_fields")
+        document = Document.objects.create(**validated_data)
+        for data in document_fields:
+            field_id = data['field_id']
+            template = TemplateField.objects.get(id=field_id.id).template_id
+            if document.template_id == template: # Эту проверку надо в валидатор засунуть. Проверяется, принадлежит ли поле выбраному шаблону
+                field = DocumentField.objects.create(field_id=field_id, value=data['value'])
+                FieldToDocument.objects.create(fields=field, document=document)            
+        return document
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        """ Пока говно код. Обновление документа и полей документа """
+        document_fields = validated_data.pop("document_fields")
+        Document.objects.filter(id=instance.id).update(**validated_data)
+        document = Document.objects.get(id=instance.id)
+        FieldToDocument.objects.filter(document=document).delete()
+        for data in document_fields:
+            field_id = data['field_id']
+            template = TemplateField.objects.get(id=field_id.id).template_id
+            if document.template_id == template: # Эту проверку надо в валидатор засунуть. Проверяется, принадлежит ли поле выбраному шаблону
+                field = DocumentField.objects.create(field_id=data['field_id'], value=data['value'])
+                FieldToDocument.objects.create(fields=field, document=document) 
+        return instance
+
+
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -3,11 +3,14 @@ from rest_framework.permissions import AllowAny
 from django.http.response import StreamingHttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import filters, status
 
 from .serializers import (
     TemplateSerializer,
-    DocumentSerializer,
-    DocumentFieldSerializer
+    DocumentReadSerializer,
+    DocumentWriteSerializer,
+    DocumentFieldSerializer,
 )
 from documents.models import (
     Document,
@@ -46,7 +49,7 @@ class TemplateFieldViewSet(viewsets.ModelViewSet):
 class DocumentViewSet(viewsets.ModelViewSet):
     """ Заглушка. Документ. """
     queryset = Document.objects.all()
-    serializer_class = DocumentSerializer
+    serializer_class = DocumentReadSerializer
     http_method_names = (
         'get',
         'post',
@@ -55,12 +58,48 @@ class DocumentViewSet(viewsets.ModelViewSet):
     )
     permissions_classes = (AllowAny,)
 
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"] and self.request.user.is_authenticated:
+            return DocumentReadSerializer
+        return DocumentWriteSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+    @action(
+        detail=False,
+        permission_classes=[AllowAny, ],
+        url_path=r'draft'
+    )
+    def draft_documents(self, request):
+        """Возвращает список незаконченных документов/черновиков"""
+        user = self.request.user
+        print(user)
+        queryset = Document.objects.filter(completed=False, owner=user)
+        serializer = DocumentReadSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(
+        detail=False,
+        permission_classes=[AllowAny, ],
+        url_path=r'history'
+    )
+    def history_documents(self, request):
+        """Возвращает список законченных документов/история"""
+        user = self.request.user
+        print(user)
+        queryset = Document.objects.filter(completed=True, owner=user)
+        serializer = DocumentReadSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     @action(
         detail=True,
         permission_classes=[AllowAny, ],
         url_path=r'download_document'
     )
     def download_document(self, request, pk=None):
+        """ Пока говно код. Скачивание готового документа"""
 
         document = get_object_or_404(Document, id=pk)
         context = dict()
@@ -75,8 +114,8 @@ class DocumentViewSet(viewsets.ModelViewSet):
         buffer = doc.get_draft(context)
 
         response = StreamingHttpResponse(
-        streaming_content=buffer,  # use the stream's content
-        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            streaming_content=buffer,  
+            content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
         )
         name = document.template_id.name
         response['Content-Disposition'] = f'attachment;filename={name}.docx'
@@ -86,9 +125,9 @@ class DocumentViewSet(viewsets.ModelViewSet):
 
 
 class DocumentFieldViewSet(viewsets.ModelViewSet):
-    """ Заглушка. Поле документа. """
+    """ Заглушка. Поле шаблона. """
     queryset = DocumentField.objects.all()
-    serializer_class = DocumentSerializer
+    serializer_class = DocumentFieldSerializer
     http_method_names = (
         'get',
         'post',
