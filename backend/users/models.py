bc682e521759812eded77b01f78b9818527e6c06
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2023-10-18 14:48:20 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,34 +8,11 @@ from .validators import validator_username
 
 class User(AbstractUser):
 
-    bio = models.TextField("Биография", null=False, blank=True)
-    username = models.CharField(
-        unique=True,
-        max_length=150,
-        validators=[
-            ASCIIUsernameValidator(),
-            validator_username,
-        ],
-    )
-    first_name = models.CharField(
-        max_length=150,
-    )
-    last_name = models.CharField(
-        max_length=150,
-    )
-    password = models.CharField(
-        max_length=150,
-    )
     email = models.EmailField(
         verbose_name="email address",
         max_length=254, unique=True)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["username", "email"],
-                name="unique_user")
-        ]
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
