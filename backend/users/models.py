fe142f5fe541c50f6382413b4113933359d25085
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2023-10-18 14:48:43 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.validators import ASCIIUsernameValidator
-from django.core.exceptions import ValidationError
 from django.db import models
-
-from .validators import validator_username
 
 
 class User(AbstractUser):
