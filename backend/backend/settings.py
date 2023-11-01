ff60516b57a4fb70993a9f3c17ca1b4dc48eebc4
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2023-11-01 15:51:44 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -176,3 +176,6 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ORIGIN_WHITELIST = [
+          'http://localhost:3000',
+      ]
