8f10e8a95c21694b3e4b125e1c873e736455b4b5
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2023-11-14 19:18:59 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,8 +117,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "collected_static"
 
 STATICFILES_DIRS = ((BASE_DIR / "static/"),)
-
-INITIAL_DATA_DIR = BASE_DIR / "data"
+INITIAL_DATA_DIR = BASE_DIR / "static/data/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/app/media"
