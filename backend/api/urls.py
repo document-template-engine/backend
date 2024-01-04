d26e1b901faf47a39e731edfd72b30474322797f
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2024-01-04 14:48:57 +0400

    2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 from django.conf.urls import url
 
 urlpatterns = [
-    path("", include("api.v1.urls")),
+    # path("v1/", include("api.v1.urls")),
     path("v2/", include("api.v2.urls")),
 ]
 
