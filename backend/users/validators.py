b35edfded963280c7bea1ea74640b67dc9017798
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2024-03-13 09:50:22 +0300

    Update validators.py

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -8,10 +8,20 @@ from django.core.exceptions import ValidationError
 # Один из спецсимволов (!#$%*&@^)
 # Длина поля:
 # Min 8 символов
-# Max 32 символов
+# Max 32 символов 
 
 def validator_password(value):
+
     if value.len() > 8 and value < 32:
         raise ValidationError("Пароль должен сожержать больше 8 или меньше 32 символов")
-    # if re.search((r'\d'), value) != ""
+    
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'
+    if re.match(pattern, value) is None:
+        raise ValidationError('Password has incorrecr format.')
+    
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'
+    if re.match(pattern, value) is None:
+        raise ValidationError('Password has incorrecr format.')
+    
     return value
+
