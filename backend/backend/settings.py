a78f460630e999204abf32bff831d1d1f8db05be
Paymir <nikox12@mail.ur>
2024-04-20 11:57:52 +0300

    add all auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,24 +188,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-# if os.getenv("BD") == "sqlite":
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("BD") == "sqlite":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": os.getenv("POSTGRES_DB", "django"),
-#             "USER": os.getenv("POSTGRES_USER", "django"),
-#             "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-#             "HOST": os.getenv("DB_HOST", ""),
-#             "PORT": int(os.getenv("DB_PORT", "5432")),
-#         }
-#     }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB", "django"),
+            "USER": os.getenv("POSTGRES_USER", "django"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+            "HOST": os.getenv("DB_HOST", ""),
+            "PORT": int(os.getenv("DB_PORT", "5432")),
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
